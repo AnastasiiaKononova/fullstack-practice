6a8cfe7dc37dc0d50c021aca58372f2be645f984
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-04-08 15:21:09 +0300

    TODO

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,15 +1,48 @@
-import axios from 'axios';
+import axios from "axios";
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:5000/api'
-})
+  baseURL: "http://localhost:5000/api",
+});
 
-export const signIn = async(data) => await httpClient.post('/users/sign-in', data);
+httpClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      // маємо додати до запиту заголовок Authorization з цим токеном
+      config.headers = {
+        ...config.headers,
+        Authorization: `Bearer ${token}`,
+      };
+    }
+    return config;
+  },
+  (err) => {
+    Promise.reject(err);
+  }
+);
 
-export const signUp = async(data) => await httpClient.post('/users/sign-up', data);
+/// TODO: response interceptors
+// Якщо приходить 403 помилка - намагаємося оновити сесію за допомогою RefreshToken
+
+/* Auth API */
+export const signIn = async (data) =>
+  await httpClient.post("/users/sign-in", data);
+
+export const signUp = async (data) =>
+  await httpClient.post("/users/sign-up", data);
+
+/* Chat API */
+
+export const getUserChats = async () => await httpClient.get("/chats");
+
+/*
+GET http://localhost:5000/api/chats HTTP/1.1
+Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NWU1OTVhNGI2ZTIwZjBiYTMyMzY5MWMiLCJlbWFpbCI6ImphbmVEZWVlb2VAcmUuZmQiLCJpYXQiOjE3MDk2MzAzMTYsImV4cCI6MTcwOTYzMDY3Nn0.xmK-wWAcbAjZ4_Q0iJcK6JovZCH2b2_h_IMK4VKB6mU
+
+*/
 
 //const API_BASE = 'http://localhost:5000/api';
- 
+
 //  export const signIn = async (data) => {
 //      const options = {
 //          method: 'POST',
@@ -21,8 +54,7 @@ export const signUp = async(data) => await httpClient.post('/users/sign-up', dat
 //      const res =  await fetch(`${API_BASE}/users/sign-in`, options);
 //      return await res.json();
 //  }
- 
- 
+
 //  export const signUp = async (data) => {
 //      const options = {
 //          method: 'POST',
