4617154c5f8ddcf374470d492fed5558b45bddb0
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-04-10 17:01:31 +0300

    Chat layout

---
+++ client/src/components/Chat/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Chat.module.css';
+const Chat = () => {
+    return (
+        <section className={styles.chat}>
+            Chat
+        </section>
+    );
+}
+
+export default Chat;
--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,13 +1,21 @@
 // Сторінка чату
- 
-import React from 'react';
- 
+
+import React from "react";
+import DialogList from "../../components/DialogList";
+import Chat from "../../components/Chat";
+import MessageArea from "../../components/MessageArea";
+import styles from "./Dashboard.module.css";
+
 const Dashboard = () => {
-    return (
-        <div>
-            Dashboard
-        </div>
-    );
-}
+  return (
+    <main className={styles["messenger-wrapper"]}>
+      <DialogList />
+      <section className={styles.container}>
+        <Chat />
+        <MessageArea />
+      </section>
+    </main>
+  );
+};
 
 export default Dashboard;
