988cd435b200e5b0ba55ab7a793185927ab57a5f
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-04-15 18:11:03 +0300

    Task DialogList resolve

--- client/src/components/MessageArea/index.js
+++ client/src/components/MessageArea/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './MessageArea.module.css';
 
+
+// зчитувати нове повідомлення і відправляти його ДашБорду, а той - відправляти на сервер
 const MessageArea = () => {
     return (
         <form className={styles.container}>
