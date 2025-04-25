498cecbecd1072faa5d05a4bc7fe00f3f1796aa2
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-04-25 19:40:43 +0300

    add new message

---
+++ client/src/actions/actionCreators.js
@@ -0,0 +1,5 @@
+import ACTION_TYPES from "./actionTypes";
+export const addNewMessage = (payload) => ({
+  type: ACTION_TYPES.ADD_NEW_MESSAGE,
+  payload,
+});
--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,50 +1,40 @@
 // Сторінка чату
-
-import React, { useState, useContext} from "react";
+import React, { useState, useContext } from "react";
+import { connect } from "react-redux";
 import DialogList from "../../components/DialogList";
 import Chat from "../../components/Chat";
 import MessageArea from "../../components/MessageArea";
 import styles from "./Dashboard.module.css";
 import ChatContext from "../../contexts/ChatContext";
-import { addNewMessage } from "../../api/index";
+// import { addNewMessage } from "../../api/index";
 import UserContext from "../../contexts/UserContext";
+import { addNewMessage } from "../../actions/actionCreators";
 
 const Dashboard = () => {
-  const [currentChat, setCurrentChat] = useState();
-  const user = useContext(UserContext);
+  // const [currentChat, setCurrentChat] = useState();
+  // const user = useContext(UserContext);
 
   const sendNewMessage = (text) => {
     const apiObj = {
-        chatId: currentChat?._id,
-        message: {
-            author: user._id,
-            body: text
-        }
+      chatId: props.currentChat?._id,
+      message: {
+        author: props.user._id,
+        body: text,
+      },
     };
-    addNewMessage(apiObj)
-    .then(({data: {data}}) => {
-        const chat = {
-            ...currentChat,
-            messages: [...currentChat.messages, data]
-        };
-        setCurrentChat(chat)
-    })
-}
+    props.addMessage(apiObj);
+    // };
+    // props.dispatch(addNewMessage(apiObj))
+    // addNewMessage(apiObj)
+    // .then(({data: {data}}) => {
+    //     const chat = {
+    //         ...currentChat,
+    //         messages: [...currentChat.messages, data]
+    //     };
+    //     setCurrentChat(chat)
+    // })
+  };
 
-
-
-/*
- 
- MessageArea task
- 
- 1. Бек done
- 2. Прописати запит на api для створення нового повідомлення
- 3. Компонента MessageArea за відправки форми має передати Dashboard інфу, а той - робити запит на відправку повідомлення. Юзера беремо з контекста, chatId з іншого контексту (CurrentChat Context)
- 4. Після повернення об'єкту з новим повідомленням (в разі успішного його додавання до БД), додати його до масиву повідомлень, який рендерить <Chat />
- 
- */
-
- 
   return (
     <ChatContext.Provider value={[currentChat, setCurrentChat]}>
       <main className={styles["messenger-wrapper"]}>
@@ -58,8 +48,18 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard;
+// export default Dashboard;
 
+const mapStateToProps = ({ user, currentChat }) => ({ user, currentChat });
 
+// const mapDispatchToProps = () => {
+//     return {
+//         addMessage: (data) => dispatch(addNewMessage(data))
+//     }
+// }
 
+const mapDispatchToProps = {
+  addMessage: addNewMessage,
+};
 
+export default connect(mapStateToProps)(Dashboard);
