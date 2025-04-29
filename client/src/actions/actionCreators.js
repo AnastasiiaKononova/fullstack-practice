bcd4f50b4d2da34f24385362463c72291c660bc7
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-04-29 18:23:49 +0300

    saga flow

--- client/src/actions/actionCreators.js
+++ client/src/actions/actionCreators.js
@@ -1,4 +1,24 @@
 import ACTION_TYPES from "./actionTypes";
+
+
+/* Auth/UserData actions */
+
+export const getUserDataRequest = () => ({
+  type: ACTION_TYPES.GET_USER_DATA_REQUEST
+})
+
+export const getUserDataSuccess = (payload) => ({
+  type: ACTION_TYPES.GET_USER_DATA_SUCCESS,
+  payload
+})
+
+export const getUserDataError = (error) => ({
+  type: ACTION_TYPES.GET_USER_DATA_ERROR,
+  error
+})
+
+
+/* Chat actions */
 export const addNewMessage = (payload) => ({
   type: ACTION_TYPES.ADD_NEW_MESSAGE,
   payload,
@@ -7,3 +27,13 @@ export const addNewMessage = (payload) => ({
 export const getUserChatList = () => ({
   type: ACTION_TYPES.GET_USER_CHATS_LIST_REQUEST
 })
+
+export const getUserChatListSuccess = (payload) => ({
+  type: ACTION_TYPES.GET_USER_CHATS_LIST_SUCCESS,
+  payload
+});
+
+export const getUserChatListError = (error) => ({
+  type: ACTION_TYPES.GET_USER_CHATS_LIST_ERROR,
+  error
+});
--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,46 +1,64 @@
 import ACTION_TYPES from "../actions/actionTypes";
-import {produce} from 'immer';
+import { produce } from "immer";
 
 const initialStates = {
   user: null,
   currentChat: [],
   error: null,
-  chatList: []
+  chatList: [],
+  isFetching: false,
 };
 
 function reducer(state = initialStates, action) {
   // Pure function!
   console.log(action);
   switch (action.type) {
-    case "GET_USER_DATA": {
-      return {
-        ...state,
-        user: action.payload,
-      };
-    }
-    case "USER_DATA_ERROR_FETCHING": {
-      return {
-        ...state,
-        error: action.error.message,
-      };
-    }
     case ACTION_TYPES.ADD_NEW_MESSAGE: {
       const nextState = produce(state, (draft) => {
         draft.currentChat.push(action.payload);
       });
       return nextState;
-      
+
       // return {
       //   ...state,
       //   currentChat: {
       //     ...state.currentChat,
       //     messages: [...state.currentChat.messages, action.payload]
-        
+
       //   }
       // }
     }
+    case ACTION_TYPES.GET_USER_CHATS_LIST_SUCCESS: {
+      return {
+        ...state,
+        isFetching: false,
+        chatList: action.payload,
+      };
+    }
+    case ACTION_TYPES.GET_USER_DATA_ERROR:
+    case ACTION_TYPES.GET_USER_CHATS_LIST_ERROR: {
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error,
+      };
+    }
+    case ACTION_TYPES.GET_USER_DATA_REQUEST:
+    case ACTION_TYPES.GET_USER_CHATS_LIST_REQUEST: {
+      return {
+        ...state,
+        isFetching: true,
+      };
+    }
+    case ACTION_TYPES.GET_USER_DATA_SUCCESS: {
+      return {
+        ...state,
+        isFetching: false,
+        user: action.payload,
+      };
+    }
     default:
-      return state; 
+      return state;
   }
 }
 export default reducer;
