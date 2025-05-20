bbd7bf8b43c080f230782cf26b42da3667f9d0ed
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-05-20 11:45:45 +0300

    WebSocket server side

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 const http = require('http');
+const {Server} = require('socket.io');
 const app = require('./app');
+const {createWebSocketConnect} = require('./websocket');
 
 const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+});
+
+io.on('connection', createWebSocketConnect)
 
 const PORT = 5000;
 
